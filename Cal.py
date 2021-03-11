72226e266d9d9110daa8f9a494443db1a4f01328
gagandeep327 <78936681+gagandeep327@users.noreply.github.com>
2021-03-11 17:32:38 -0500

    First commit

---
+++ Cal.py
@@ -0,0 +1,17 @@
+
+
+StudentInfo = {
+"class": "first",
+"student": "Gagan",
+"name":"Mike",
+"marks":{
+        "physics":70,
+        "history":80
+              }
+         }
+
+
+physicsMarks = StudentInfo["class"]["student"]["marks"]["physics"]
+historyMarks = StudentInfo["class"]["student"]["marks"]["history"]
+
+print(physics + historyMarks)
